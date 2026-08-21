e202e7d82ff354967d886ad1219953bf5974461c
PW170 <pw170@users.noreply.github.com>
2026-08-21 20:05:00 +0530

    Remove temporary python script files

--- fix_chestesp.py
+++
@@ -1,7 +0,0 @@
-with open("src/main/java/com/github/scoliossis/modules/impl/render/ChestESP.java", "r") as f:
-    c = f.read()
-
-c = c.replace("ColorUtil.resetColor()", "com.github.scoliossis.utils.tenacity.render.TRenderUtil.resetColor()")
-
-with open("src/main/java/com/github/scoliossis/modules/impl/render/ChestESP.java", "w") as f:
-    f.write(c)
--- rename_arraylist2.py
+++
@@ -1,9 +0,0 @@
-import codecs
-with codecs.open('src/main/java/com/github/scoliossis/modules/impl/client/ArrayListModule.java', 'r', 'utf-8') as f:
-    code = f.read()
-
-code = code.replace('"ArrayListWidget",', '"ArrayListWidget2",')
-
-with codecs.open('src/main/java/com/github/scoliossis/modules/impl/client/ArrayListModule.java', 'w', 'utf-8') as f:
-    f.write(code)
-print("done")
